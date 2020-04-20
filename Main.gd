da122db60941f93fcb1c6edf019f4b5e0abeb25f
Sam Feng <yfeng68@ucsc.edu>
2020-04-20 14:45:49 -0600

    Initial commit

---
+++ Main.gd
@@ -0,0 +1,35 @@
+extends Node
+
+
+# Position Tracking
+###################
+
+# the current position of the song (in seconds)
+var songPosition: float
+
+# the current position of the song (in beats)
+var songPosInBeats: float
+
+# the duration of a beat
+var secPerBeat: float
+
+# how much time (in seconds) has passed since the song started
+var dsptimesong: float
+
+
+# Song Information
+###################
+
+# beats per minute of a song
+var bpm: float
+
+# keep all the position-in-beats of notes in the song
+# note formar: (type, start, end, xloc, yloc)
+
+
+
+func _ready():
+	# calculate how many seconds is one beat
+	secPerBeat = 60.0 / bpm
+
+
