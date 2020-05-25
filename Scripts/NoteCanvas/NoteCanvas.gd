54a331bddb6ca65f8b14f02208a842e2c3cc1401
Sam Feng <yfeng68@ucsc.edu>
2020-05-24 22:36:46 -0600

    Grid Snapping & Waveform Fixes

--- Scripts/NoteCanvas/NoteCanvas.gd
+++ Scripts/NoteCanvas/NoteCanvas.gd
@@ -8,6 +8,11 @@ var mouseEntered = false
 var blockLines = []
 var beatLines = []
 
+var snappingThreshold = 23.0
+
+var selectedBlock = 0
+var selectedBeat = 0
+
 
 func _ready():
 	viewportRect = get_viewport_rect()
@@ -15,22 +20,40 @@ func _ready():
 	# Calculate Block Lines
 	for i in range(1, 10):
 		var blockYPos: float = viewportRect.size.y * 0.8 * (float(i) / 10.0)
-		blockLines.append([Vector2(0, blockYPos), Vector2(viewportRect.size.x, blockYPos), Color("2e3840"), 2])
+		blockLines.append([viewportRect.size.x, blockYPos, Color("2e3840"), 2])
 
 
 func _process(_delta):
-	#update()
-	pass
+	
+	if (mouseEntered):
+		
+		var mousePos = get_viewport().get_mouse_position()
+		
+		for i in range(blockLines.size()):
+			if abs(blockLines[i][1] - mousePos.y) < snappingThreshold:
+				blockLines[i][2] = Color("1cff9b")
+				selectedBlock = i + 1
+			else:
+				blockLines[i][2] = Color("2e3840")
+			
+			
+		update()
 
 
 func _draw():
+	
 	# Draw Block Lines
 	for line in blockLines:
-		draw_line(line[0], line[1], line[2], line[3])
+		draw_line(
+			Vector2(0, line[1]), 
+			Vector2(line[0], line[1]), 
+			line[2], line[3])
 	
-	if (mouseEntered):
-		var mousePos = get_viewport().get_mouse_position()
-		print(mousePos)
+	# Draw Judgement Line
+	draw_line(
+		Vector2(viewportRect.size.x * 0.1, 0), 
+		Vector2(viewportRect.size.x * 0.1, viewportRect.size.y),
+		Color("ffffff"), 2)
 
 
 # Capture mouse when entered
@@ -39,3 +62,11 @@ func _on_ViewportContainer_mouse_entered():
 
 func _on_ViewportContainer_mouse_exited():
 	mouseEntered = false
+
+
+# Detect click events
+func _on_ViewportContainer_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			print(selectedBlock, selectedBeat)
+		
--- Scripts/NoteCanvas/WaveformRender.gd
+++ Scripts/NoteCanvas/WaveformRender.gd
@@ -3,24 +3,38 @@ extends Node2D
 
 onready var viewportRect = get_viewport_rect()
 
-var AudioData: PoolByteArray
+onready var sectionHeight = 0.5
+onready var sectionLocation = viewportRect.size.y * 0.9
 
+var AudioData: PoolByteArray
+	
 
 func _process(_delta):
 	update()
 
 
 func _draw():
-	# Testing Audio Data Drawing
 	var sampleSize: int = (len(AudioData) / viewportRect.size.x) * EditorDatas.scaleFactor
+	
+	# Temp check!!! Need REMOVE
 	if len(AudioData) > 100:
-		for i in range(0, viewportRect.size.x - 1):
-			# Convert Signed Byte to Correct Int rep
-			var barPos = AudioData[int(i * sampleSize)]
-			if barPos > 127:
-				barPos = (256 - barPos) * (-1)
+		
+		for i in range(0, viewportRect.size.x):
 			
-			draw_line(Vector2(i, 200 + barPos), Vector2(i+1, 200 + barPos + 2), Color("1cff9b"), 2)
+			# Check if index out of bound
+			if int(i * sampleSize) > len(AudioData):
+				break
+				
+			var barPos = AudioData[int(i * sampleSize)]
+			
+			# Convert Signed Byte to Correct Int rep
+			if barPos > 127:
+				barPos = (-1) * (256 - barPos)
+		
+			draw_line(
+				Vector2(i, sectionLocation - barPos * sectionHeight), 
+				Vector2(i, sectionLocation + barPos * sectionHeight), 
+				Color("1cff9b"), 2)
 
 
 # Testing Audio Data Drawing
